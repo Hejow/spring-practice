2dac46b8066e9741743773ea9105d266dd32fda1
Moon He Jow <gmlwh124@naver.com>
2022-08-26 14:24:03 +0900

    practice thymeleaf

--- ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
+++ ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -5,6 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.ui.Model;
+import org.zerock.ex3.dto.SampleDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 @RequestMapping("/sample")
 @Log4j2
@@ -12,5 +20,20 @@ public class SampleController {
     @GetMapping("/ex1")
     public void ex1() {
         log.info("ex1........");
+    }
+
+    @GetMapping("/ex2")
+    public void exModel(Model model) {
+        List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i -> {
+            SampleDTO dto = SampleDTO.builder()
+                    .sno(i)
+                    .first("First.."+i)
+                    .last("Last.."+i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+            return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("list", list);
     }
 }
