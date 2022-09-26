28ec037d59968fe1e4d15e6971263273b254035e
Moon He Jow <gmlwh124@naver.com>
2022-09-27 03:10:13 +0900

    feat: Add Entities

---
+++ mreview/src/main/java/org/zerok/mreview/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package org.zerok.mreview.entity;
+
+import lombok.Getter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = AutoCloseable.class)
+@Getter
+abstract class BaseEntity {
+    @CreatedDate
+    @Column(name = "regdate", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name = "moddate")
+    private LocalDateTime modDate;
+}
