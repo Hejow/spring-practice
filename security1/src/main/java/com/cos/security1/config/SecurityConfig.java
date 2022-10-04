4de1c1535c11779a6b0f4e1f2c1bf2f077b6fad8
Moon He Jow <gmlwh124@naver.com>
2022-10-04 17:19:46 +0900

    Spring Security Init

---
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 활성화 시 스프링 시큐리티 필터가 필터체인에 등록
+public class SecurityConfig { // << 이게 바로 필터
+
+    // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasAnyRole('ROLE_MANAGER','ROLE_ADMIN')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                // 권한없는 페이지를 접근할 때 로그인 페이지로 이동
+                .and()
+                .formLogin()
+                .loginPage("/loginForm");
+
+        return http.build();
+    }
+}
