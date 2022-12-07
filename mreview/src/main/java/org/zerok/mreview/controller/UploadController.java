f0f4bd2cc99776d5329e6740548eff1d022c391a
Moon He Jow <gmlwh124@naver.com>
2022-12-07 16:21:42 +0900

    feat: FileUpload

---
+++ mreview/src/main/java/org/zerok/mreview/controller/UploadController.java
@@ -0,0 +1,70 @@
+package org.zerok.mreview.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@RestController
+@Log4j2
+public class UploadController {
+    private final String uploadPath;
+
+    public UploadController(@Value("${org.zerock.upload.path}") String uploadPath) {
+        this.uploadPath = uploadPath;
+    }
+
+    @PostMapping("/uploadAjax")
+    public void uploadFile(MultipartFile[] uploadFiles) {
+        for (MultipartFile uploadFile : uploadFiles) {
+            if (uploadFile.getContentType().startsWith("image") == false) {
+                log.warn("this file is not image type // filename : " + uploadFile.getOriginalFilename());
+                return;
+            }
+
+            String originalName = uploadFile.getOriginalFilename();
+            String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
+
+            log.info("fileName : " + fileName);
+            // 날짜 폴더 생성
+            String folderPath = makeFolder();
+
+            // UUID
+            String uuid = UUID.randomUUID().toString();
+
+            // 저장할 파일 이름 중간에 "_"를 이용해서 구분
+            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+
+            Path savePath = Paths.get(saveName);
+
+            try {
+                uploadFile.transferTo(savePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String makeFolder() {
+        String str = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if (uploadPathFolder.exists() == false) {
+            uploadPathFolder.mkdirs();
+        }
+        return folderPath;
+    }
+}
